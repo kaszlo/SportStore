7d1a3a93ed39c933180610e7e1368cb001767ad3
Sebastian Olszak <s.olszak@hotmail.com>
2018-01-13 12:33:04 +0100

    initial commit

---
+++ SportStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repo;
+
+        public ProductController(IProductRepository repository)
+        {
+            repo = repository;
+        }
+
+        public ViewResult List() => View(repo.Products);
+    }
+}
